cfc1e57c5ad64e2c6a177bedb827aaee44ed434f
Peter <peter@penseesoft.com>
2019-04-23 15:40:34 +0800

    修改格式

--- csproj2bat.js
+++ csproj2bat.js
@@ -7,16 +7,19 @@ var util=require("./lib/pathEx.mjs");
 
 
 
-function Main(fileName,binDir)
-{
+function Main(fileName,binDir,options){
 
     let xml = fs.readFileSync(fileName, 'utf8');
     let xmlDoc = new dom().parseFromString(xml);
     let analyzer = new lib.ProjectFileAnalyzer(xmlDoc);
 
-    let dllFileNames = analyzer.GetDllFiles();
-    let csFiles =  analyzer.GetCSFiles();
-    let assemblyName = analyzer.GetAssemblyName();
+    let dllFileNames = analyzer.getDllFiles();
+    let csFiles = null;
+    if(options.outputCs)
+    {
+        csFiles =  analyzer.getCSFiles();
+    }
+    let assemblyName = analyzer.getAssemblyName();
 
     let batName = util.reExtName(fileName,"bat");
     //console.log(batName);
@@ -29,37 +32,51 @@ function Main(fileName,binDir)
 set csc=%windir%\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe
 set bin=${binDir}
 
-%csc% /out:`
+%csc%  /target:library 
+/out:`
             );
             fs.writeSync(fd,assemblyName);
             fs.writeSync(fd,".dll");   
             fs.writeSync(fd," ^\r\n"); 
 
-        csFiles
-            .map(x=>`${x} ^\r\n`)
-            .forEach(x=>fs.writeSync(fd,x));
-        
+        if (csFiles !=null)
+        {
+            csFiles
+                .map(x=>`${x} ^\r\n`)
+                .forEach(x=>fs.writeSync(fd,x));
+        }
+
         fs.writeSync(fd,"/r:");
 
         dllFileNames
             .map(x=>`%bin%${path.basename(x)},^\r\n`)
             .forEach(x=>{fs.writeSync(fd,x)});
 
-        fs.writeSync(fd," /target:library /nologo  \r\n");    
+        fs.writeSync(fd," /nologo  \r\n");    
         fs.writeSync(fd,"pause");
         fs.close(fd,()=>{});
         
     });
 }
 
-
+class ToBatOption
+{
+    constructor(outputCs = true)
+    {
+        this.outputCs = outputCs;
+    }
+}
 
 let filename=process.argv[2];
 let binDir = process.argv[3];
+
+let option =new ToBatOption(true);
 
 if(filename ===undefined)
     console.error("input csproj file name");
 else 
 {    
-    Main(filename,binDir);
+    Main(filename,binDir,option);
 }
+
+
--- test2Bat.bat
+++ test2Bat.bat
@@ -1,1 +1,1 @@
-node csproj2bat.js "D:\Project_All\Gits\v10_Branch\V10.3.10.0\Attendance2\Penseesoft.Attendance.TestApi.Web\Penseesoft.Attendance.TestApi.Web.csproj" d:\bin\
+node csproj2bat.js "D:\Project_All\Gits\v10_Branch\V10.4.2\WebService\Web\Penseesoft.WebService.Web.csproj" d:\bin\
